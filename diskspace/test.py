cad3d1ef58a55a0e9799005801d3fe917b6e9236
Thiago <thiagoiferreira@outlook.com>
2018-06-21 12:24:09 -0300

    test_subprocess_check_output() functional and passing

--- diskspace/test.py
+++ diskspace/test.py
@@ -1,32 +1,28 @@
 from diskspace import bytes_to_readable, print_tree, show_space_list,subprocess_check_output
 import unittest
-<<<<<<< HEAD
 import subprocess
+import os
 
 class TestMethods(unittest.TestCase):
-=======
-
-
-class TestStringMethods(unittest.TestCase):
->>>>>>> b060faeaec42e12fc9ab72ea3a0f163b079b1ba4
 
     def test_bytes_to_readable(self):
         blocks = 10
         result = (blocks/2)
         result = str(format(result,'.2f'))+'Kb'
         self.assertEqual(bytes_to_readable(blocks),result)
-<<<<<<< HEAD
-    #def test_subprocess_check_output(self):
-        #string = "/unittest tecprog"
-        #result = subprocess.check_output(string.strip().split(' '))
-        #self.assertEqual(result,['/unittest','tecprog'])
-=======
-    def test_subprocess_check_output(self):
-        string = "/unittest tecprog"
-        result = subprocess_check_output(string)
-        self.assertEqual(result,['/unittest'])
->>>>>>> b060faeaec42e12fc9ab72ea3a0f163b079b1ba4
     
+    def test_subprocess_check_output(self):
+        depth = -1
+        abs_directory = os.path.abspath('.')
+        string = 'du '
+        if depth != -1:
+            string += '-d {} '.format(depth)
+        
+        string += abs_directory
+        result = subprocess.check_output(string.strip().split(' '))
+        self.assertEqual(subprocess_check_output(string),result)
+    
+            
 #    def test_print_tree(self):
 
  #       self.assertEqual()
